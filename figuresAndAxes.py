ea4e766b598bee82e246fc3ae4b112829a74cf39
Chike Nwaenie <cn4g18@soton.ac.uk>
2021-10-24 14:42:44 +0100

    Added legends, fixed bugs

--- figuresAndAxes.py
+++ figuresAndAxes.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-fig1, (ax1, ax2) = plt.subplots(nrows=2)
+
+fig1, (ax1, ax2) = plt.subplots(nrows=2, sharex=True)
 fig2, ax3 = plt.subplots()
 
 def f(x):
@@ -19,13 +20,20 @@ functionspaceF = f(sets)
 functionspaceG = g(sets)
 functionspaceH = h(sets)
 
-fig1.title("Polynomial curves")
-fig2.title("Linear curves")
+ax1.set_title("Polynomial curves")
+ax1.set_ylabel("y")
+
+ax2.set_xlabel("x")
+ax2.set_ylabel("y")
+
+ax3.set_xlabel("x")
+ax3.set_ylabel("y")
+ax3.set_title("Linear curves")
+
+ax1.plot(sets, functionspaceF, label="f(x) = x^2")
+ax2.plot(sets, functionspaceG, label="f(x) = 0")
+ax3.plot(sets, functionspaceH, label="f(x) = x")
 
 ax1.legend()
 ax2.legend()
 ax3.legend()
-
-ax1.plot(sets, functionspaceF, label="f(x) = x^2", shareX=True)
-ax2.plot(sets, functionspaceG, label="f(x) = 0")
-ax3.plot(sets, functionspaceF, label="f(x) = x")
