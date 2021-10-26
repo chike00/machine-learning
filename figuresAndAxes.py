2adfd9850f92073e73490ad0474204a8651a3209
Chike Nwaenie <cn4g18@soton.ac.uk>
2021-10-26 14:00:23 +0100

    Added a scatter graph

--- figuresAndAxes.py
+++ figuresAndAxes.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 fig1, (ax1, ax2) = plt.subplots(nrows=2, sharex=True)
 fig2, ax3 = plt.subplots()
+fig3, ax4 = plt.subplots()
 
 def f(x):
   return x**2
@@ -31,9 +32,23 @@ ax3.set_ylabel("y")
 ax3.set_title("Linear curves")
 
 ax1.plot(sets, functionspaceF, label="f(x) = x^2")
-ax2.plot(sets, functionspaceG, label="f(x) = 0")
+ax2.plot(sets, functionspaceG, label="f(x) = x^3")
 ax3.plot(sets, functionspaceH, label="f(x) = x")
 
 ax1.legend()
 ax2.legend()
 ax3.legend()
+
+def sinx(x):
+    return np.sin(x)
+
+N = 10
+setSin = np.linspace(-np.pi, np.pi, N)
+funcSin = sinx(setSin)
+
+ax4.scatter(setSin, funcSin, c="r")
+ax4.plot(setSin, funcSin, "-", c="b", label="f(x)  = sin(x)")
+ax4.set_xlabel("x, in radians")
+ax4.set_ylabel("y")
+ax4.set_title("Scatter graph of sin(x)")
+ax4.legend()
